9294520d802d7b3e2bbf784e6894dbac67f6d4cf
Robin Rainton <robin@rainton.com>
2022-08-28 17:53:08 +0200

    #329 Added type, unit, role where known

--- smartmeter.js
+++ smartmeter.js
@@ -539,6 +539,44 @@ function processMessage(obj) {
     }
 }
 
+// Types, units of TIC fields
+// TODO: add descriptions with translations
+// TODO: add tri-phase
+// TODO: add 'standard' mode
+const ticStateCommon = {
+    'ADCO': { type: 'string' },
+    'OPTARIF': { type: 'string' },
+    'ISOUSC': { type: 'number', unit: 'A', role: 'value.current' },
+    'BASE': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+
+    'HCHC': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'HCHP': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+
+    'EJPHN': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'EJPHPM': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+
+    'BBRHCJB': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'BBRHPJB': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'BBRHCJW': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'BBRHPJW': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'BBRHCJR': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+    'BBRHPJR': { type: 'number', unit: 'Wh', role: 'value.power.consumption' },
+
+    'PEJP': { type: 'number' /* In minutes */ },
+
+    'PTEC': { type: 'string' },
+    'DEMAIN': { type: 'string' },
+
+    'IINST': { type: 'number', unit: 'A', role: 'value.current' },
+    'ADPS': { type: 'number', unit: 'A', role: 'value.current' },
+    'IMAX': { type: 'number', unit: 'A', role: 'value.current' },
+
+    'PAPP': { type: 'number', unit: 'VA' }, // TODO: no role for VA documented
+
+    'HHPHC': { type: 'string' },
+    'MOTDETAT': { type: 'string' }
+};
+
 function processTic(smOptions) {
     adapter.log.debug('Starting TIC processing');
 
@@ -562,52 +600,61 @@ function processTic(smOptions) {
 
         const parts = data.split(/\s+/);
         const name = parts.shift();
+        // convert string value to number if necessary
         const value = parts.shift();
         const checksum = parts.shift();
         // TODO: verify checksum;
 
+        // TODO: Does the protocol cater for multiple ADCO values in the same stream?
         if (name === 'ADCO') {
             adco = value;
             adapter.log.info('Found ID ' + adco);
             setConnected(true);
         }
 
-        if (nameValueCache[name] === value) {
+        if (ticStateCommon[name] == undefined) {
+            // We don't know what this field is so ignore it
+            adapter.log.warn('Unknown label (ignoring): ' + name);
+        } else if (nameValueCache[name] === value) {
             // Nothing has changed since the last time, do nothing
-        } else {
+        } else if (adco) {
+            // Label known & new value so create channel/state  set value
             nameValueCache[name] = value;
 
-            if (adco) {
-                // Only create/set objects if the channel (adco) is known.
-                // TODO: this needs cleaning up
-                adapter.setObjectNotExists(adco, {
-                    typeisSet: 'channel',
-                    common: {
-                        name: adco
-                    },
-                    native: {}
-                }, (err) => {
-                    if (err) {
-                        adapter.log.error('Failed to create channel ' + adco);
-                    } else {
-                        const stateName = adco + '.' + name;
-                        adapter.setObjectNotExists(stateName, {
-                            type: 'state'
-                            // TODO: other attributes?
-                        }, (err) => {
-                            if (err) {
-                                adapter.log.error('Failed to create state ' + stateName);
-                            } else {
-                                adapter.setState(stateName, { ack: true, val: value }, (err) => {
-                                    if (err) {
-                                        adapter.log.error('Failed to set state ' + stateName);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+            // Only create/set objects if the channel (adco) is known.
+            // TODO: this needs cleaning up
+            adapter.setObjectNotExists(adco, {
+                type: 'channel',
+                common: {
+                    name: adco,
+                },
+                native: {}
+            }, (err) => {
+                if (err) {
+                    adapter.log.error('Failed to create channel ' + adco);
+                } else {
+                    const stateName = adco + '.' + name;
+                    adapter.setObjectNotExists(stateName, {
+                        type: 'state',
+                        common: ticStateCommon[name]
+                        // TODO: other attributes?
+                    }, (err) => {
+                        if (err) {
+                            adapter.log.error('Failed to create state ' + stateName);
+                        } else {
+                            // convert string value to number if necessary
+                            adapter.setState(stateName, {
+                                ack: true,
+                                val: ticStateCommon[name].type == 'number' ? Number(value) : value
+                            }, (err) => {
+                                if (err) {
+                                    adapter.log.error('Failed to set state ' + stateName);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     });
 }
